6c9c840b261766d79b39db8cfc07b9e903056079
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-09 00:32:56 -0500

    Update

---
+++ Goodbye.js
@@ -0,0 +1,9 @@
+function goodbye(name) {
+    // your code here...
+   return "Bye " + name; 
+  }
+  
+  console.log(goodbye("Daniel")); // => "Bye Daniel."
+  console.log(goodbye("Mark")); // => "Bye Mark."
+  console.log(goodbye("Beyonce")); // => "Bye Beyonce."
+  
