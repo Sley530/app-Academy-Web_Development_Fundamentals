e0f30af91451690e70f9df43d7fea22de75228b2
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-09 01:55:02 -0500

    Update

---
+++ Plus_5.js
@@ -0,0 +1,13 @@
+/* in this function I will
+ * Write a function plusFive that takes in
+ * a number as an argument and returns 
+ * the sum of that number and 5.
+ */
+
+function plusFive(num) {
+    return num + 5;
+  }
+  
+  console.log(plusFive(10));  // => 15
+  console.log(plusFive(2));   // => 7
+  console.log(plusFive(-8));  // => -3
