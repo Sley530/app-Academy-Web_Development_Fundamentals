965c6539f754ae29811b3af96e90dae8871d2927
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-09 02:05:59 -0500

    Update

---
+++ Average_of_2.js
@@ -0,0 +1,15 @@
+/* In this exercise I will
+ * Write a method averageOfTwo(num1, num2)
+ * that returns the average of two numbers.
+ *
+ * @author Sley
+ */
+
+function averageOfTwo(num1, num2) {
+    let avg = (num1 + num2) / 2;
+    return avg;
+  }
+  
+  console.log(averageOfTwo(3, 7)); // => 5.0
+  console.log(averageOfTwo(16, 5)); // => 10.5
+  console.log(averageOfTwo(2, 7.5)); // => 4.75
