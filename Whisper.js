81302b936465d893cf3e266cb958eee988948f12
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-09 02:20:04 -0500

    Update

---
+++ Whisper.js
@@ -0,0 +1,13 @@
+/* In this exercise I will
+ * Write a function whisper that takes in 
+ * a string and returns a "whispered" version of that string.
+ * 
+ * @author Sley
+ */ 
+
+function whisper(str) {
+    return str.TolowerCase();
+  }
+  
+  console.log(whisper("Hey Buddy")); // => "...hey buddy..."
+  console.log(whisper("YEA! that was fun")); // => "...yea! that was fun..."
