3d5ea8ee011be4bc6c183ae42273263be9de87b5
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-09 02:36:05 -0500

    Update

--- Yell.js
+++ Yell.js
@@ -12,3 +12,4 @@ function yell(str) {
   
   console.log(yell("I want to go to the store")); // => "I WANT TO GO TO THE STORE!!!"
   console.log(yell("Time to program")); // => "TIME TO PROGRAM!!!"
+  
