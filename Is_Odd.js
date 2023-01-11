fa59b756839da41ab742440755f4b4deac0f6481
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-10 22:14:46 -0500

    Update

--- Is_Odd.js
+++ Is_Odd.js
@@ -12,27 +12,27 @@
    * and one without using conditionals.
    **/
 
-function isOdd(num) {
-    if (num % 2 === 0) 
-    {
-        return false;
-    } 
+// function isOdd(num) {
+//     if (num % 2 === 0) 
+//     {
+//         return false;
+//     } 
     
-        return true;
-  }
+//         return true;
+//   }
   
-  console.log(isOdd(2)); // => false
-  console.log(isOdd(5)); // => true
-  console.log(isOdd(-17)); // => true
+//   console.log(isOdd(2)); // => false
+//   console.log(isOdd(5)); // => true
+//   console.log(isOdd(-17)); // => true
 
 
  // Without conditionals.
 
  function isOdd(num) {
-     num % 2 === 0;
-    return true;
- }
+    num % 2 === 0;
     return false;
+ }
+    return true;
   
     console.log(isOdd(2)); // => false
     console.log(isOdd(5)); // => true
