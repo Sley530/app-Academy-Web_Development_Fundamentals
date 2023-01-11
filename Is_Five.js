38276cc867e5cd208373195594f17e7bddb31b3c
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-10 21:42:13 -0500

    Update

--- Is_Five.js
+++ Is_Five.js
@@ -6,8 +6,16 @@
  */ 
 
 function isFive(num) {
-
+    if (num === 5) 
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
 }
+
 
 console.log(isFive(5)); // => true
 console.log(isFive(13)); // => false
