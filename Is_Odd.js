fe9e1695a4a10712bb4682127deaf94dbbd4a27e
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-10 21:58:19 -0500

    Update

---
+++ Is_Odd.js
@@ -0,0 +1,19 @@
+/* In this exercise I will
+ * Write a function isOdd that takes in 
+ * a number as an argument and returns true if the number is odd 
+ * and returns false otherwise.
+ *
+ * @author Sley
+ */
+
+function isOdd(num) {
+    if (num % 2 == 1) {
+        return false;
+    } else {
+        return true;
+    }
+  }
+  
+  console.log(isOdd(2)); // => false
+  console.log(isOdd(5)); // => true
+  console.log(isOdd(-17)); // => true
