f08d474fb60130a8c8781a223dc192aee6d8fccb
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-10 22:59:33 -0500

    Update

---
+++ Is_Substring.js
@@ -0,0 +1,16 @@
+/* In this exercise I will
+ * Write a function isSubstring that takes in two strings, 
+ * searchString and subString. The function should 
+ * return true if subString is a part of thesearchString, 
+ * regardless of upper or lower case, and false if otherwise.
+ * 
+ * @author Sley
+ */
+
+function isSubstring(searchString, subString) {
+    // your code here...
+  }
+  
+  console.log(isSubstring("The cat went to the store", "he cat went")); // => true
+  console.log(isSubstring("Time to program", "time")); // => true
+  console.log(isSubstring("Jump for joy", "joys")); // => false
