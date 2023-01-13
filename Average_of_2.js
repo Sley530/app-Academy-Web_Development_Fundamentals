312990de77fe29c60a8c011cceef15ea3e7d591b
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-12 22:36:27 -0500

    Update

--- Average_of_2.js
+++ Average_of_2.js
@@ -5,10 +5,11 @@
  * @author Sley
  */
 
-function averageOfTwo(num1, num2) {
+function averageOfTwo(num1, num2) 
+{
     let avg = (num1 + num2) / 2;
     return avg;
-  }
+}
   
   console.log(averageOfTwo(3, 7)); // => 5.0
   console.log(averageOfTwo(16, 5)); // => 10.5
