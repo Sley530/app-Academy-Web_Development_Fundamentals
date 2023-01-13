ac91969563bd6699e007545c43b27e79a096fa40
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-13 17:17:21 -0500

    Update

---
+++ Log_Between.js
@@ -0,0 +1,19 @@
+/* In this exercise I will
+ * Define a function logBetween(lowNum, highNum) 
+ * that will print every number from lowNum to highNum, inclusive. 
+ * Inclusive means that the range includes lowNum and highNum.
+ * 
+ * @author Sley
+ */
+
+function logBetween(lowNum, highNum) 
+{
+    if(lowNum < highNum)
+    { 
+        for(let i = 0; i <= (highNum - lowNum); i++)
+        {
+            console.log(lowNum + i);
+        } 
+        
+    }
+}
