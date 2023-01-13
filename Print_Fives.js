0311bae22a1130d4366296d848acd31b1c6969d2
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-13 17:31:41 -0500

    Update

---
+++ Print_Fives.js
@@ -0,0 +1,16 @@
+/* In this exercise I will
+ * Write a function printFives(max) 
+ * that prints out the multiples of 5 that are less than the max.
+ * 
+ * @author Sley
+ */
+
+function printFives(max) 
+{
+    for (let i = 0; i < max; i*=5)
+    {
+        console.log(i);
+    }
+} 
+
+printFives(20);
