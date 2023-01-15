22a4082b561b05b959e6fb327b18ff40782669f7
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 18:08:52 -0500

    Update

---
+++ Fizzbuz.js
@@ -0,0 +1,12 @@
+/* In this exercise, I will
+ * Define a function fizzBuzz(max) that takes a number 
+ * and prints every number from 0 to max (not inclusive) 
+ * that is divisible by either 3 or 5, but not both.
+ * 
+ * @author Sley
+ */
+
+function fizzBuzz(max) 
+{
+    
+}
