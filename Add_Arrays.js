7f14d395f01c668b95aae990a9230636704987b6
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 10:29:27 -0500

    Update

--- Add_Arrays.js
+++ Add_Arrays.js
@@ -6,7 +6,7 @@
  */
 
 function combineArray(array1, array2) {
-    return array1.concat(array2);
+    return (array1.concat(array2));
 }
 
 console.log(combineArray([1, 2], [3, 4])); // => [1, 2, 3, 4]
