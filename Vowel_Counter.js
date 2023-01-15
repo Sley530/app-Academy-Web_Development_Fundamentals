61ab9376a753a4dacb797b9b0c454f7ca128d21d
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 16:55:27 -0500

    Update

--- Vowel_Counter.js
+++ Vowel_Counter.js
@@ -8,10 +8,18 @@
 
 function countVowels(word) 
 {
-   
+   let MyArray = ["a", "e", "i", "o", "u"];
+
+   let i = 0
+   while (i < MyArray.length)
+   {
+        return MyArray.length;
+        
+   }
+
+    
 };
-  
+
   console.log(countVowels("bootcamp")); // => 3
-  console.log(countVowels("apple")); // => 2
-  console.log(countVowels("pizza")); // => 2
-  
+//   console.log(countVowels("apple")); // => 2
+//   console.log(countVowels("pizza")); // => 2
