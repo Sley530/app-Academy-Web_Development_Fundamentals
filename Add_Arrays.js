e9e9a2ceb8875b0e91093c9e2faa2731be6dba38
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 10:28:43 -0500

    Update

---
+++ Add_Arrays.js
@@ -0,0 +1,13 @@
+/* In this exercise I will
+ * Write a function combineArray(array1, array2) that takes in two arrays of numbers 
+ * and returns the two arrays combined into a single array.
+ * 
+ * @author Sley
+ */
+
+function combineArray(array1, array2) {
+    return array1.concat(array2);
+}
+
+console.log(combineArray([1, 2], [3, 4])); // => [1, 2, 3, 4]
+console.log(combineArray([17, 5], [6, 7]));  // => [17, 5, 6, 7]
