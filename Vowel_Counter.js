9c4ec4281dadcdb2d897967c2ad803697ddb71f6
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 13:15:52 -0500

    Update

---
+++ Vowel_Counter.js
@@ -0,0 +1,17 @@
+/* In this exercise I will
+ * Write a function, countVowels(word), that takes in a string word 
+ * and returns the number of vowels in the word.
+ * Vowels are the letters "a", "e", "i", "o", "u".
+ * 
+ * @author Sley
+ */
+
+function countVowels(word) 
+{
+   
+};
+  
+  console.log(countVowels("bootcamp")); // => 3
+  console.log(countVowels("apple")); // => 2
+  console.log(countVowels("pizza")); // => 2
+  
