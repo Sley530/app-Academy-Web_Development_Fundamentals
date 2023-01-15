f9c4f3bbd09e348fa460d5b0daee18f2871f6f4a
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-14 22:23:06 -0500

    Update

---
+++ Log_Between_Stepper.js
@@ -0,0 +1,12 @@
+/* In this exercise I will
+ * Write a function logBetweenStepper(min, max, step) 
+ * that takes in 3 numbers as parameters. 
+ * The function should print out numbers between min and max at step intervals.
+ * 
+ * @author Sley
+ */
+
+function logBetweenStepper(min, max, step)
+{
+    
+}
