192b6d5a81060384b7c4365c484f90105b1416eb
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 13:30:54 -0500

    Update

---
+++ Devide_by_3.js
@@ -0,0 +1,15 @@
+/* In this exercise I will
+ * Write a method divideByThree(num)
+ * that returns the passed in number argument divided by three.
+ * 
+ * @author Sley
+ */ 
+
+function divideByThree(num) {
+    return num / 3;
+  }
+  
+  console.log(divideByThree(9)); // => 3.0
+  console.log(divideByThree(15)); // => 5.0
+  console.log(divideByThree(98)); // => 32.666666666666664
+  
