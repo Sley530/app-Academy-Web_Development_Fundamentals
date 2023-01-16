ca15afde0f5bcb7704b8ab4b99a96625996005da
Sley530 <paul-wesleyjeanty@my.uopeople.edu>
2023-01-15 19:01:44 -0500

    Update

--- Fizzbuz.js
+++ Fizzbuz.js
@@ -8,5 +8,25 @@
 
 function fizzBuzz(max) 
 {
-    
+    let i = 1;
+    while (i < max){
+        if (((i % 3 === 0) && !(i % 5 === 0)) 
+        || (!(i % 3 === 0) && (i % 5 === 0)))
+        {
+            console.log(i);   
+        }
+        
+        i++;
+        
+    }
+
 }
+
+fizzBuzz(20); // prints out:
+// 3
+// 5
+// 6
+// 9
+// 10
+// 12
+// 18
